f246a357bd8ea307b8ac2144f6b972ff0065d1c2
Baidaodao <43870788+Baidaodao@users.noreply.github.com>
2022-09-17 13:11:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "5e6420138a5eac52675feed5ee28b2e3",
+  APP_SECRET: "c87c836c731ed92dcff02cf16167790e",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
